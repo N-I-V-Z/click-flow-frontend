a9e3e008a939a8a5233125f58f1054890c65c76a
hocdethanhdev <tritrann03@gmail.com>
2025-03-10 23:44:44 +0700

    add roles publisher

--- src/routes/publisherRoutes.tsx
+++ src/routes/publisherRoutes.tsx
@@ -1,9 +1,23 @@
 import { Navigate } from 'react-router-dom';
-import NotFound from '@/pages/NotFound';
+import { Suspense, lazy } from 'react';
+import PublisherLayout from '@/components/layout/PublisherLayout';
 
-const publicRoutes = [
-  { path: '/404', element: <NotFound /> },
-  { path: '*', element: <Navigate to="/404" replace /> }
+const PublisherDashboard = lazy(() => import('@/pages/Publisher/index'));
+
+const PublisherRoutes = [
+  {
+    path: '/publisher',
+    element: (
+      <Suspense fallback={<div>Loading Publisher Layout...</div>}>
+        <PublisherLayout />
+      </Suspense>
+    ),
+    children: [
+      { index: true, element: <Navigate to="dashboard" replace /> },
+      { path: 'dashboard', element: <PublisherDashboard /> }
+      // Các route con khác nếu có...
+    ]
+  }
 ];
 
-export default publicRoutes;
+export default PublisherRoutes;
